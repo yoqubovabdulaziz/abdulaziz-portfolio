11ab7a511f880ec4e0b896f704664cb24ce7d323
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2024-05-29 03:31:54 +0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = () => {
+    return (
+        <>
+            <section id="hero">
+                <div className="container hero"></div>
+            </section>
+        </>
+    )
+}
+
+export default Home
